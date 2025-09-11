f14f0e757de45253481d3bd17c6d699ba1eea3ef
Gavin Elster <gavin.elster@me.com>
2025-09-11 14:21:56 -0700

    Add example of enableAppSwitcherProtectionAsync bug

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { enableAppSwitcherProtectionAsync } from 'expo-screen-capture'
+import { useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  useEffect(() => {
+    enableAppSwitcherProtectionAsync()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Demo of enableAppSwitcherProtectionAsync race condition. If you move the app to the background and back quickly, multiple blur layers will be created over the app. But only the last one will get removed, causing an unrecoverable state.</Text>
+      <Text style={styles.text}>To fix, the iOS module’s showPrivacyOverlay needs a guard statement to make it idempotent.</Text>
     </View>
   );
 }
@@ -17,4 +22,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    maxWidth: 400,
+    fontSize: 18,
+    marginBottom: 18,
+  }
 });
